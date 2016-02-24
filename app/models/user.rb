8d51f7bcc9ba1f33911f54bb34187f81228a143e
dinh phu <dinhphu94@gmail.com>
2016-02-24 07:48:51 +0700

    refs #54839 create admin account by using rake task

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
     :trackable, :validatable
+
+  enum role: [:admin, :user]
 
   has_many :user_books
   has_many :favorites
