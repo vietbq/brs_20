2d4c122c125a14932c9a5f855d146d6b795d9082
dinh phu <dinhphu94@gmail.com>
2016-02-24 15:41:12 +0700

    Refs #54584 admin can create new book

---
+++ app/controllers/admin/books_controller.rb
@@ -0,0 +1,21 @@
+class Admin::BooksController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+    @categories = Category.all
+  end
+
+  def create
+    if @book.save
+      flash[:success] = t "flash.admin.book.create_book"
+      redirect_to admin_books_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit :title, :content, :author, :publish_date, :number_of_pages, :category_id
+  end
+end
