7df66ae8065baf3fd16f8dce745463dad5cd5d99
Ngoc Vu <ngocvu.uet@gmail.com>
2016-02-25 07:44:50 +0700

    edit and delete book

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,12 +1,12 @@
 class Admin::BooksController < ApplicationController
   load_and_authorize_resource
+  before_action :load_categories, only: [:new, :edit]
 
   def index
     @books = Book.paginate page: params[:page], per_page: Settings.books.page
   end
 
   def new
-    @categories = Category.all
   end
 
   def create
@@ -18,7 +18,32 @@ class Admin::BooksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @book.update_attributes book_params
+      flash[:success] = t "admin.books.updated_success"
+      redirect_to [:admin, @book]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @book.destroy
+      flash[:success] = t "admin.books.destroy-success"
+    else
+      flash[:alert] = t "admin.books.destroy-fail"
+    end
+    redirect_to [:admin, @book]
+  end
+
   private
+  def load_categories
+    @categories = Category.all
+  end
+
   def book_params
     params.require(:book).permit :title, :content, :author, :publish_date, :number_of_pages, :category_id
   end
